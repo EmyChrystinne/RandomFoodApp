539721e6918b8f5671c0e37a04f6343d4f3aad05
Emylle Chrystinne <emychrystinne@gmail.com>
2024-04-10 09:43:40 -0300

    page surprise styling

--- randomfoodapp/src/components/SurpriseSelection.js
+++ randomfoodapp/src/components/SurpriseSelection.js
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../styles/Surprise.css";
 
 const SurpriseSelection = () => {
-  const [SurpriseSelection, setRandomRestaurant] = useState(null);  
+  const [SurpriseSelection, setRandomRestaurant] = useState(null);
 
   useEffect(() => {
     const fetchRandomRestaurant = async () => {
-      console.log('fetchRandomRestaurant');
+      console.log("fetchRandomRestaurant");
       try {
-        const response = await axios.get('http://localhost:3001/api/restaurants/random');
+        const response = await axios.get(
+          "http://localhost:3001/api/restaurants/random"
+        );
         setRandomRestaurant(response.data);
       } catch (error) {
-        console.error('Erro ao obter restaurante aleatório:', error);
+        console.error("Erro ao obter restaurante aleatório:", error);
       }
     };
 
@@ -21,35 +24,52 @@ const SurpriseSelection = () => {
 
   const handleRefresh = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/api/restaurants/random');
+      const response = await axios.get(
+        "http://localhost:3001/api/restaurants/random"
+      );
       setRandomRestaurant(response.data);
     } catch (error) {
-      console.error('Erro ao obter restaurante aleatório:', error);
+      console.error("Erro ao obter restaurante aleatório:", error);
     }
   };
 
   return (
-    <div>
-      <h2>Seleção Surpresa</h2>
-      {SurpriseSelection ? (
-        <div>
-          <p><strong>Nome:</strong> {SurpriseSelection.NOME}</p>
-          <p><strong>Categoria:</strong> {SurpriseSelection.CATEGORIA}</p>
-          <p><strong>Preço Médio:</strong> {SurpriseSelection.PREÇO_MEDIO}</p>
-          <p><strong>Refeição:</strong> {SurpriseSelection.REFEIÇÃO}</p>
-          <p><strong>Localização:</strong> {SurpriseSelection.LOCALIZAÇÃO}</p>
+    <div className="surprisePage">
+      <div className="content">
+        <h2>Seleção Surpresa</h2>
+        {SurpriseSelection ? (
+          <div>
+            <p>
+              <strong>Nome:</strong> {SurpriseSelection.NOME}
+            </p>
+            <p>
+              <strong>Categoria:</strong> {SurpriseSelection.CATEGORIA}
+            </p>
+            <p>
+              <strong>Preço Médio:</strong> {SurpriseSelection.PREÇO_MEDIO}
+            </p>
+            <p>
+              <strong>Refeição:</strong> {SurpriseSelection.REFEIÇÃO}
+            </p>
+            <p>
+              <strong>Localização:</strong> {SurpriseSelection.LOCALIZAÇÃO}
+            </p>
+          </div>
+        ) : (
+          <p>Carregando restaurante aleatório...</p>
+        )}
         </div>
-      ) : (
-        <p>Carregando restaurante aleatório...</p>
-      )}
-      <button onClick={handleRefresh}>Simbora!</button>
+      <div className="buttons">
+        <Link to="/home">
+          <button id="go" className="button">SIMBORA!</button>
+        </Link>
+        <button onClick={handleRefresh} className="button" id="outro">Que tal outro?</button>
+      </div>
     </div>
   );
-}
+};
 
 export default SurpriseSelection;
-
-
 
 // import React from 'react';
 
